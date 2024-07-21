ef8876cdea62bbac6caeb1fbe590ff076ce7132c
Matt-Shing-927 <mattshing927@gmail.com>
2024-07-21 14:43:47 +1200

    added items to world + locks

--- Scenes/Objects/PlayerDeath.gd
+++ Scenes/Objects/PlayerDeath.gd
@@ -3,7 +3,10 @@ extends PlayerState
 func enter() -> void:
 	player.velocity.x = 0
 	player.velocity.y = 0
-	$"../../CollisionShape2D".queue_free()
+	var collider = get_parent().get_parent().get_node_or_null("CollisionShape2D")
+	print(collider)
+	if collider:
+		collider.queue_free()
 
 func exit() -> void:
 	pass
