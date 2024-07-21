c4c681cce63b4ca7404b45c2df256fc6a8cf7e2f
Anton Garay <antonvince.garay@gmail.com>
2024-07-21 16:06:52 +1200

    THE FINAL STAND

---
+++ Scripts/Cutscenes/endingscene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	animation_player.play("ending_scene_1")
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func on_end() -> void:
+	TransitionManager.flash_screen()
+	SoundManager.play_sound("MagnumShot")
+	await get_tree().create_timer(1).timeout
+	TransitionManager.change_scene("res://Scenes/Cutscenes/endingtwo.tscn")
+	
--- Scripts/GlobalSingletons/TransitionManager.gd
+++ Scripts/GlobalSingletons/TransitionManager.gd
@@ -20,3 +20,8 @@ func flash_screen() -> void:
 	await $AnimationPlayer.animation_finished
 	$AnimationPlayer.play_backwards("transition")
 	flash.visible = false
+
+func end_game() -> void:
+	flash.visible = true
+	$AnimationPlayer.play("END-GAME")
+	get_tree().quit()
